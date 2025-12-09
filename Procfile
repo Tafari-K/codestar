6904174861bbfee5c1523ee921d2d3edff371e3e
Tafari-K <kirton.tafari@gmail.com>
2025-12-09 19:14:16 +0000

    install apps, update settings, DEBUG - False

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn codestar-djproject.wsgi
--- codestar/settings.py
+++ codestar/settings.py
@@ -23,9 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--rjrrj5q7gs2ju+*0m(qqy!5+a9pmy1by&&_^8a5r88q(ve!k='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '.herokuapp.com',
+    '127.0.0.1',
+]
 
 
 # Application definition
