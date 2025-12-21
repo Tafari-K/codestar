f7cb7e0a66ae2be09c1f8ea9251ae850f1a9fd04
Tafari-K <kirton.tafari@gmail.com>
2025-12-21 16:23:55 +0000

    Updated model logic to resolve render issue

--- about/models.py
+++ about/models.py
@@ -5,7 +5,7 @@ from django.db import models
 
 class About(models.Model):
     title = models.CharField(max_length=200)
-    update_on = models.DateTimeField(auto_now=True)
+    updated_on = models.DateTimeField(auto_now=True)
     content = models.TextField()
 
     def __str__(self):
