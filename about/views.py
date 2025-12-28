2c5a8a056a7d5e1e69624ff0d015e2f55303b4d4
Tafari-K <kirton.tafari@gmail.com>
2025-12-28 17:13:29 +0000

    Added collaboration form to about page and handling logic

--- about/views.py
+++ about/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.contrib import messages
 from .models import About
 from .forms import CollaborateForm
 
@@ -11,6 +12,13 @@ def about_me(request):
     """
     about = About.objects.all().order_by('-updated_on').first()
     collaborate_form = CollaborateForm()
+
+    if request.method == "POST":
+        collaborate_form = CollaborateForm(data=request.POST)
+        if collaborate_form.is_valid():
+            collaborate_form.save()
+            collaborate_form = CollaborateForm()
+            messages.add_message(request, messages.SUCCESS, "Collaboration request receieved! I endeavor to respond within 2 working days.")
 
     return render(
         request,
